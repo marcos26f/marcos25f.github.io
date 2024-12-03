685da3b6c31d16042e87ea4d27550492d1d5415a
marcus <marcosviniciospaesf32@gmail.com>
2024-12-03 20:57:00 -0300

    criaçao da pagina de login

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-document.getElementById("toggle-form").addEventListener("click", function() {
-    const loginForm = document.getElementById("login-form");
-    const signupForm = document.getElementById("signup-form");
-    const formTitle = document.getElementById("form-title");
+document.getElementById('toggle-form').addEventListener('click', function() {
+    const loginForm = document.getElementById('login-form');
+    const signupForm = document.getElementById('signup-form');
+    const formButton = document.getElementById('toggle-form');
 
     if (loginForm.style.display === "none") {
         loginForm.style.display = "flex";
         signupForm.style.display = "none";
-        formTitle.textContent = "Login";
-        this.textContent = "Cadastrar";
+        formButton.textContent = "Cadastrar"; // Botão que volta ao cadastro
     } else {
         loginForm.style.display = "none";
         signupForm.style.display = "flex";
-        formTitle.textContent = "Cadastro";
-        this.textContent = "Voltar ao Login";
+        formButton.textContent = "Voltar"; // Botão que volta ao login
     }
 });
